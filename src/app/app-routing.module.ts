9822eba0e47faf0340447a80f9fe97d2b95e971f
Fernando Elias <ferelias_1077@hotmail.com>
2023-10-06 02:36:43 -0600

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent }
+    ])
+  ],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
